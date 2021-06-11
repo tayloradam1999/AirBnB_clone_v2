98848d4c93f8094248204b0204ff6cb18b900f42
JakeFC <jacobchavera@yahoo.com>
2021-06-11 19:27:19 +0000

    added fw slashes before data directory name

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,12 +3,12 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-sudo mkdir -p data/web_static/releases/test
-sudo mkdir -p data/web_static/shared
-sudo touch data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+sudo touch /data/web_static/releases/test/index.html
 sudo echo "<html>\n\t<head></head>\n\t<body>Testing</body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
-sudo mkdir -p data/web_static/current
-sudo ln -sfn data/web_static/releases/test data/web_static/current
+sudo mkdir -p /data/web_static/current
+sudo ln -sfn /data/web_static/releases/test /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data
 # editing nginx
 find='^\tlocation / {'
