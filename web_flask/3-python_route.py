3f2a20fd8bd65e4bf6b91a5fd26d1e68096ca055
Adam Taylor <tayloradam1999@gmail.com>
2021-06-19 02:44:45 +0000

    Pushing file for task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+Write a script that starts a Flask web application
+"""
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    return "C %s" % str(text.replace("_", " "))
+
+
+@app.route("/python/<string:text>", strict_slashes=False)
+@app.route("/python/")
+def python(text="is_cool"):
+    return "Python %s" % str(text.replace("_", " "))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
