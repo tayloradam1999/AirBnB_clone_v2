a34a6457a5f74620cb4bd1ff86af8e715a09c713
tayloradam1999 <tayloradam1999@gmail.com>
2021-06-19 22:10:08 +0000

    Pushing files for task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from models import storage
+from models.state import State
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def tearDown(self):
+    storage.close()
+
+@app.route("/states_list", strict_slashes=False)
+def statesList():
+    objs = storage.all(State)
+    obj_list = sorted(objs.values(), key=lambda x: x.name)
+    return render_template('7-states_list.html', s=obj_list)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
