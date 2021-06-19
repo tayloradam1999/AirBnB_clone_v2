1490d27ee6782949a2c0495b132f50a5d334105f
Adam Taylor <tayloradam1999@gmail.com>
2021-06-19 03:15:44 +0000

    Pushing files and new dir for task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""
+Write a script that starts a Flask web application
+"""
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    return "C %s" % str(text.replace("_", " "))
+
+
+@app.route("/python/<string:text>", strict_slashes=False)
+@app.route("/python/")
+def python(text="is_cool"):
+    return "Python %s" % str(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def intCheck(n):
+    return "%d is a number" % (n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def intCheckTemplate(n):
+    return render_template('5-number.html', value=n)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
