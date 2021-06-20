0f5f9b9d6f9510fefaad3b5fcf350c98065431d1
Adam Taylor <tayloradam1999@gmail.com>
2021-06-20 01:10:38 +0000

    Pushing pep8 changes and added documentation

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 
+"""
+Lists all State objects present in DBStorage
+sorted by name (A-Z)
+"""
+
 from models import storage
 from models.state import State
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
 def tearDown(self):
     storage.close()
+
 
 @app.route("/states_list", strict_slashes=False)
 def statesList():
